5745f8c1213eb5d093f53c365cc5bbee5879732a
YummyMirror <anatolianiukevich@gmail.com>
2018-06-12 22:35:43 +0300

    Have added Precondition to Logi test

--- project_1/src/test/java/test/Testik.java
+++ project_1/src/test/java/test/Testik.java
@@ -3,14 +3,19 @@ package test;
 import base.TestBase;
 import model.User;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class Testik extends TestBase {
 
+    @BeforeMethod
+    public void preCondition() {
+        app.navigateTo().mainPage();
+    }
+
     @Test
     public void test() {
-        app.navigateTo().mainPage();
         app.login().loginAs(new User().setLogin("admin")
                                       .setPassword("secret"));
 
